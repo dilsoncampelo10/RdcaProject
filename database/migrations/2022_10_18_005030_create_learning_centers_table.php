524bf038c231c31ecce4b6073b7276f752f4e71b
dilsoncampelo10 <dilsono316@gmail.com>
2022-10-17 22:01:56 -0300

    databases

---
+++ database/migrations/2022_10_18_005030_create_learning_centers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('learning_centers', function (Blueprint $table) {
+            $table->id();
+            $table->string('discipline');
+            $table->dateTime('datetime');
+            $table->string('place');
+            $table->char('format');
+            $table->foreignId("id_teacher")->references("id")->on("teachers");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('learning_centers');
+    }
+};
